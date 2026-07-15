96f21f816f27c27384cd0594b002fe352a1a1fa2
GermanViter <228225241+GermanViter@users.noreply.github.com>
2026-07-15 00:10:27 -0400

    set cursor on start

--- caelestia/.config/caelestia/hypr-user.lua
+++ caelestia/.config/caelestia/hypr-user.lua
@@ -4,19 +4,19 @@ hl.env("HYPRCURSOR_SIZE", "30")
 hl.env("XCURSOR_THEME", "Bibata-Modern-Ice")
 hl.env("XCURSOR_SIZE", "30")
 
-hl.on("hyprland.start", function ()
-    hl.exec_cmd("hyprctl setcursor Bibata-Modern-Ice 30")
-    hl.exec_cmd("hyprctl reload")
+hl.on("hyprland.start", function()
+	hl.exec_cmd("hyprctl setcursor Bibata-Modern-Ice 30")
+	hl.exec_cmd("hyprctl reload")
 end)
 
 misc = {
-    vrr = 0
+	vrr = 0,
 }
 
 hl.monitor({
-    output  = "DP-2",
-    mode    = "2560x1440@144",
-    scale   = 1,
+	output = "DP-2",
+	mode = "2560x1440@144",
+	scale = 1,
 })
 
 hl.bind("SUPER + h", hl.dsp.focus({ direction = "left" }))
@@ -29,4 +29,4 @@ hl.bind("SUPER + SHIFT + l", hl.dsp.window.move({ direction = "right" }))
 hl.bind("SUPER + SHIFT + k", hl.dsp.window.move({ direction = "up" }))
 hl.bind("SUPER + SHIFT + j", hl.dsp.window.move({ direction = "down" }))
 
-hl.bind( "CTRL + ALT + Delete", hl.dsp.exec_cmd("pkill Hyprland"))
+hl.bind("CTRL + ALT + Delete", hl.dsp.exec_cmd("pkill Hyprland"))
