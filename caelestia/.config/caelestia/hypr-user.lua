aea854f0bc658f7aa6dc41f16b3bcfd29af93ba4
GermanViter <german.viter@icloud.com>
2026-08-13 10:57:24 -0400

    disable full screen transparency

--- caelestia/.config/caelestia/hypr-user.lua
+++ caelestia/.config/caelestia/hypr-user.lua
@@ -32,4 +32,5 @@ hl.bind("SUPER + SHIFT + j", hl.dsp.window.move({ direction = "down" }))
 
 hl.bind("CTRL + ALT + Delete", hl.dsp.exec_cmd("pkill Hyprland"))
 
+hl.window_rule({ match = { class = "brave-browser", fullscreen = false }, opacity = "0.8 override" })
 hl.window_rule({ match = { class = "brave-browser" }, opacity = "0.8 override" })
